336e7eab176d6da317eac24ae414eb66f7282d40
Sam Svindland <sam@Sams-MacBook-Pro.local>
2024-05-12 16:20:38 -0400

    fix 404's on insights and settings placeholder pages

---
+++ src/app/insights/page.tsx
@@ -0,0 +1,7 @@
+export default async function Insights() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1>Insights</h1>
+    </main>
+  );
+}
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,7 @@
+export default async function Settings() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1>Settings</h1>
+    </main>
+  );
+}
